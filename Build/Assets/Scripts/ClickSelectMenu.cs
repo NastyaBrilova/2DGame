3b3d0553900e4dd0a1a8dd123c95daedf0dac69b
NastyaBrilova <72560282+NastyaBrilova@users.noreply.github.com>
2022-05-16 18:18:16 +0300

    Delete Build/Assets/Scripts directory

--- Build/Assets/Scripts/ClickSelectMenu.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ClickSelectMenu : MonoBehaviour
-{
-    public int preSelectID = -1;
-    public bool[] charuse = { true, false, false, false, false };
-    public GameObject[] charCell;
-    public Sprite[] charSprites;
-}
